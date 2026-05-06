056dfa84fc8f2c51919d73573ea56c51c4ff4568
isthisjason <shuangzp@gmail.com>
2026-05-06 11:59:32 -0600

    modernized financial overview cards

--- src/components/OverviewCard.js
+++ src/components/OverviewCard.js
@@ -3,7 +3,7 @@ import './OverviewCard.css';
 
 function OverviewCard({ title, amount, color }) {
   return (
-    <div className="overview-card" style={{ borderColor: color }}>
+    <div className={`overview-card overview-card-${color}`}>
       <h3>{title}</h3>
       <p>{amount}</p>
     </div>
